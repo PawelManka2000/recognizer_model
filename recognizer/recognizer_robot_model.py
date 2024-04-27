33ad57d9209d56622c3b0db82cbe09382ec51014
PawelManka2000 <pawelmanka0@gmail.com>
2024-04-27 12:51:37 +0200

    serial communication with stm

--- recognizer/recognizer_robot_model.py
+++ recognizer/recognizer_robot_model.py
@@ -1,6 +1,27 @@
-def main():
-    print('Hi from recognizer.')
+import rclpy 
+from recognizer.drivers.MotorDriver import MotorDriver
 
+def main(args=None):
+
+
+    rclpy.init(args=args)
+
+    motor_driver = MotorDriver()
+
+    rate = motor_driver.create_rate(2)
+    while rclpy.ok():
+        # rclpy.spin_once(motor_driver)
+        motor_driver.check_encoders()
+
+
+    motor_driver.close_conn()
+    motor_driver.destroy_node()
+    rclpy.shutdown()
+
+
+# def main():
+#     print("HIHIHI ")
 
 if __name__ == '__main__':
+
     main()
